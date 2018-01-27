0b7496e17a9d36b4ec4dcec78cf13a4e24d43853
MichaelSmithwick <MichaelSmithwick@users.noreply.github.com>
2018-01-27 02:20:27 -0800

    Added the ability to find the location of the pip in space and to use it to target objects and to determine if the targeting is hitting the object.

--- BattleTank/Source/BattleTank/TankPlayerController.cpp
+++ BattleTank/Source/BattleTank/TankPlayerController.cpp
@@ -1,6 +1,7 @@
 // Copyright(c)2018 -- Mike Smithwick -- All Rights Reserved
 
 #include "TankPlayerController.h"
+#include "Engine/World.h"
 
 void ATankPlayerController::BeginPlay()
 {
@@ -19,10 +20,95 @@ void ATankPlayerController::BeginPlay()
 	}
 }
 
+void ATankPlayerController::Tick(float DeltaSeconds)
+{
+	Super::Tick(DeltaSeconds);
+
+	AimTowardsCrosshair();
+}
+
 ATank* ATankPlayerController::GetControlledTank() const
 {
 	return Cast<ATank>(GetPawn());
 }
 
+void ATankPlayerController::AimTowardsCrosshair()
+{
+	ATank* MyTank = GetControlledTank();
 
+	if (!MyTank)
+	{
+		UE_LOG(LogTemp,Error,TEXT("Unable to get Controlled Tank in AimTowardsCrosshair() function"))
+		return;
+	}
+
+	FVector HitLocation;
+//	UE_LOG(LogTemp, Warning, TEXT("Hit Location: %s"), *(HitLocation.ToString()))
+
+	if (GetSightRayHitLocation(HitLocation))
+	{
+		UE_LOG(LogTemp,Warning,TEXT("New Hit Location: %s"),*(HitLocation.ToString()))
+	}
+}
+
+bool ATankPlayerController::GetSightRayHitLocation(FVector& HitLocation) const
+{
+	// HitLocation = FVector(1.0);
+	// Find the crosshair
+	// 'De-project' the screen position of the crosshair to a world direction
+	// Linetrace along that look direction, and see what we hit (up to max range)
+
+	int32 ViewportSizeX, ViewportSizeY;
+	GetViewportSize(ViewportSizeX, ViewportSizeY);
+
+	FVector2D ScreenLocation = FVector2D(
+		((float)ViewportSizeX)*CrossHairXLocation,
+		((float)ViewportSizeY)*CrossHairYLocation
+	);
+
+	FVector LookDirection;
+	if(GetLookDirection(ScreenLocation, LookDirection))
+	{
+		return GetLookVectorHitLocation(LookDirection,HitLocation);
+	}
+
+	return false;
+}
+
+bool ATankPlayerController::GetLookDirection(const FVector2D& ScreenLocation, FVector& LookDirection) const
+{
+	FVector WorldLocation;
+	return DeprojectScreenPositionToWorld(ScreenLocation.X, ScreenLocation.Y, WorldLocation, LookDirection);
+}
+
+bool ATankPlayerController::GetLookVectorHitLocation(const FVector& LookDirection,FVector& HitLocation) const
+{
+	FHitResult OutHit;
+	FVector Start=PlayerCameraManager->GetCameraLocation();
+	FVector End = Start + (LookDirection*LineTraceRange); // note vector functionality here!!
+	/* -- This will work but the method used is more elegent
+	FVector End=FVector(
+		Start.X+(LookDirection.X*LineTraceRange),
+		Start.Y+(LookDirection.Y*LineTraceRange),
+		Start.Z+(LookDirection.Z*LineTraceRange)
+	);
+	*/
+	FCollisionQueryParams Params;
+	FCollisionResponseParams ResponseParam;
+
+	if (GetWorld()->LineTraceSingleByChannel(
+		OutHit,
+		Start,
+		End,
+		ECollisionChannel::ECC_Visibility, // hit anything that is visible
+		Params,
+		ResponseParam))
+	{
+		HitLocation = OutHit.Location;
+		return true;
+	}
+
+	HitLocation = FVector(0.0);
+	return false;
+}
 
--- BattleTank/Source/BattleTank/TankPlayerController.h
+++ BattleTank/Source/BattleTank/TankPlayerController.h
@@ -20,9 +20,36 @@ class BATTLETANK_API ATankPlayerController : public APlayerController
 	
 public:
 	virtual void BeginPlay() override;
+	virtual void Tick(float DeltaSeconds) override;
+
+private:
 
 	ATank* GetControlledTank() const;
-	
-	
-	
+
+	// Start the tank moving the barrel so that a shot would hit where
+	// the crosshair intersects the world
+	void AimTowardsCrosshair();
+
+	// Return an OUT parameter, true if hit landscape
+	bool GetSightRayHitLocation(FVector & HitLocation) const;
+
+	bool GetLookDirection(const FVector2D& ScreenLocation, FVector & LookDirection) const;
+
+	bool GetLookVectorHitLocation(const FVector& LookDirection, FVector& HitLocation) const;
+
+	// screen coordinates have 0,0 in upper left hand corner
+	// horizontal crosshair position on screen - range 0.0 to 1.0
+	UPROPERTY(EditAnywhere)
+	float CrossHairXLocation = 0.5000;
+
+	// vertical crosshair position on screen - range 0.0 to 1.0
+	UPROPERTY(EditAnywhere)
+	float CrossHairYLocation = 0.3333;
+
+	// TODO Make this parameter to be kilometers and scale programatically
+	//      to be centimeters - 1 Million cm is crazy when most calculations
+	//      are going to be in kilometers!
+	// distance in centimeters
+	UPROPERTY(EditAnywhere)
+	float LineTraceRange = 1000000.0; // 1 MILLION Centimeters!
 };
